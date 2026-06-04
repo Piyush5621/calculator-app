52a53004ed3eafeda21635bd590b064b00509ca6
Piyush <piyushkumar10902080@gmail.com>
2026-06-04 16:30:13 +0530

    Initial Maven Docker Project

---
+++ src/main/java/com/piyush/App.java
@@ -0,0 +1,13 @@
+package com.piyush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Jenkins Maven Docker Project");
+    }
+}
